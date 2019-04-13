omnirom/android_hardware_qcom_media-caf-sdm660

adjust for omni

Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-sdm660)
+
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -20,4 +22,5 @@ ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
 endif
 
+endif
 endif

whydu/android_hardware_qcom_media

Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-sdm660)
+
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -20,4 +22,5 @@ ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
 endif
 
+endif
 endif